29c2394cd03810b1ee9507d960c4647374f38dd8
krinapatel440@gmail.com <krinapatel440@gmail.com>
2019-11-17 14:16:05 -0500

    show teacherdata commit

---
+++ showTeacher.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="showTeacher.aspx.cs" Inherits="Assignment_2ab.showTeacher" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    
+    <div id="teacher_details" runat="server">
+        <h2>Complete Details ::</h2>
+        Teacher Fname: <span id="teacher_fname" runat="server"></span><br />
+        Teacher Lname: <span id="teacher_lname" runat="server"></span><br />
+        Employee Number: <span id="employee_number" runat="server"></span><br />
+        Hire Date: <span id="hire_date" runat="server"></span><br />
+        Salary: <span id="salary" runat="server"></span><br />
+    </div>
+</asp:Content>
---
+++ showTeacher.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_2ab
+{
+    public partial class showTeacher : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bool valid = true;
+            string teacherid = Request.QueryString["teacherid"];
+            if (String.IsNullOrEmpty(teacherid)) valid = false;
+            if (valid)
+            {
+                var db = new Schooldb();
+                Dictionary<String, String> student_record = db.FindStudent(Int32.Parse(teacherid));
+
+                if (student_record.Count > 0)
+                {
+                    teacher_fname.InnerHtml = student_record["TEACHERFNAME"];
+                    teacher_lname.InnerHtml = student_record["TEACHERLNAME"];
+                    employee_number.InnerHtml = student_record["EMPLOYEENUMBER"];
+                    hire_date.InnerHtml = student_record["HIREDATE"];
+                    salary.InnerHtml = student_record["SALARY"];
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!valid)
+            {
+                teacher_details.InnerHtml = "Wrong details entered!!";
+            }
+        }
+    }
+}
